97491795579a26de9a5612625b06f31197673046
jorneylopez <jorney95@live.com>
2023-04-25 15:06:41 -0600

    Counter done

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,63 +1,71 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react'
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
 
 export const CounterScreen = () => {
 
     const [count, setCount] = useState(0);
 
     return (
-        <View style={{
-            flex: 1,
-            backgroundColor: 'gray',
-            justifyContent: 'center'
-        }}>
-            <Text style={{
-                fontSize: 45,
-                textAlign: 'center',
-                top: -15
-            }}>
+        <View style={styles.container}>
+            <Text style={styles.containerCounter}>
                 Counter  {count}
             </Text>
 
             <TouchableOpacity
+                style={styles.fabLocationBR}
                 onPress={() => setCount(count + 1)}
-                style={{
-                    bottom: 10
-                }}
             >
-                <View
-                    style={{
-                        backgroundColor: 'green',
-                        borderRadius: 100,
-                        alignItems: 'center'
-                    }}
-                >
-                    <Text
-                        style={{
-                            fontSize: 25
-                        }}
-                    >+ 1</Text>
+                <View style={styles.button}>
+                    <Text style={styles.buttonText}>+ 1</Text>
                 </View>
             </TouchableOpacity>
 
             <TouchableOpacity
+                style={styles.fabLocationBL}
                 onPress={() => setCount(count - 1)}
             >
-                <View
-                    style={{
-                        backgroundColor: 'orange',
-                        borderRadius: 100,
-                        alignItems: 'center'
-                    }}
-                >
-                    <Text
-                        style={{
-                            fontSize: 25
-                        }}
-                    >- 1</Text>
+                <View style={styles.button}>
+                    <Text style={styles.buttonText}>- 1</Text>
                 </View>
             </TouchableOpacity>
 
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'gray',
+        justifyContent: 'center'
+    },
+    containerCounter: {
+        fontSize: 45,
+        textAlign: 'center',
+        top: -15
+    },
+    fabLocationBR: {
+        position: 'absolute',
+        bottom: 25,
+        right: 25
+    },
+    fabLocationBL: {
+        position: 'absolute',
+        bottom: 25,
+        left: 25
+    },
+    button: {
+        backgroundColor: '#5856D6',
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        fontSize: 25,
+        color: 'white',
+        fontWeight: 'bold',
+        alignSelf: 'center'
+    }
+});
